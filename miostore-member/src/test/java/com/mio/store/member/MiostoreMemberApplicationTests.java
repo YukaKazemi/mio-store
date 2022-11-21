81b885a5379acec31485456f4284ae644887ed09
MioSakura <1126227358@qq.com>
2022-11-21 15:47:42 +0800

    第一次提交

---
+++ miostore-member/src/test/java/com/mio/store/member/MiostoreMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mio.store.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiostoreMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ miostore-product/src/main/java/com/mio/store/product/MiostoreProductApplication.java
@@ -0,0 +1,13 @@
+package com.mio.store.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiostoreProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiostoreProductApplication.class, args);
+	}
+
+}
